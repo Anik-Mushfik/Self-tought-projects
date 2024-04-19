e6dcaff458439061ea4a1b4e2522a46794036d9d
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2024-04-19 22:04:23 +0600

    Update clock.py

--- clock.py
+++ clock.py
@@ -2,21 +2,21 @@ from tkinter import *
 from datetime import datetime as dt
 
 window = Tk()
-window.geometry("300x300")
-window.title("Clock")
+window.geometry("500x250")
+window.title("Micro Second Clock")
 window.config(background="black")
 
 
 def update_time():
-    new_time=f"Time: {dt.now().hour}:{dt.now().minute}:{dt.now().second} \nDate:{dt.now().day}/{dt.now().month}/{dt.now().year}"
+    new_time=f"Time: {dt.now().hour}:{dt.now().minute}:{dt.now().second}:{dt.now().microsecond} \nDate:{dt.now().day}/{dt.now().month}/{dt.now().year}"
     lable.config(text=new_time)
 
 
 lable = Label(
     master=window, 
-    text=f"Time: {dt.now().hour}:{dt.now().minute}:{dt.now().second} \nDate:{dt.now().day}/{dt.now().month}/{dt.now().year}", 
+    text=f"Time: {dt.now().hour}:{dt.now().minute}:{dt.now().second}:{dt.now().microsecond} \nDate:{dt.now().day}/{dt.now().month}/{dt.now().year}", 
     font="Calibri 30 bold", 
-    foreground="red", 
+    foreground="yellow", 
     background="black"
     )
 lable.pack(pady=30, side="top")
