497e150103f950f5f2b5a11e5504c58bbc8d3ec9
Musfique Ahmed <134811149+Anik-Mushfik@users.noreply.github.com>
2024-08-28 11:25:33 +0600

    Create first_try.py

---
+++ Gauss-Jordan Method/first_try.py
@@ -0,0 +1,10 @@
+""" 
+This program works like a calculator of Gauss-Jordan Method. 
+It conducts the method to get the row echelon form and 
+the reduced row echelon form. 
+
+There is already a library called sympy which does those methods and other features.
+
+This progarm does almost the same but will be making it from scratch.
+"""
+
